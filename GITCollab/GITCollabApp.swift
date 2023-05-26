c331876bd0c1f9336ed45afa2abd7ec151b7eb24
pahalayjs <pahalayjs@gmail.com>
2023-05-26 10:46:07 +0700

    Initial Commit

---
+++ GITCollab/GITCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITCollabApp.swift
+//  GITCollab
+//
+//  Created by Pahala Sihombing on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GITCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
